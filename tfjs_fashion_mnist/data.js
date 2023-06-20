528c2d477d0d6791b247e843d761d09f83681988
Hamed Nasr <76178686+hamednasr@users.noreply.github.com>
2023-06-20 09:25:36 -0400

    creating script

--- tfjs_fashion_mnist/data.js
+++ tfjs_fashion_mnist/data.js
@@ -25,7 +25,7 @@ const MNIST_LABELS_PATH =
  * NOTE: This will get much easier. For now, we do data fetching and
  * manipulation manually.
  */
-export class FMnistData {
+export class FashionMnistData {
   constructor() {
     this.shuffledTrainIndex = 0;
     this.shuffledTestIndex = 0;
